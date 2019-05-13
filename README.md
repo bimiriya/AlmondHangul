a4a544a3133189f02b144aa198e301adb966268a
pamela.oyarzun <pamela.oyarzun@cci-entel.com>
2019-05-13 02:28:23 -0400

    falta el pattern

---
+++ README.md
@@ -0,0 +1,36 @@
+# Almond Hangul
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/bimiriya/AlmondHangul/tree/master)
+### Disclaimer
+__✓__ Almond Hangul es una librería para transformar texto  en caracteres coreanos
+`✖` __No__ es una herramienta de traducción, solo de transcripción para palabras de fonética española
+
+
+ Hecho con amors &#x1F495;
+
+
+### Dependencias
+* [Hangul.js](https://github.com/e-/Hangul.js) para la construcción de las sílabas coreanas
+
+### Implementación
+
+1. Añade el script de Hangul.js antes que el de Almond Hangul
+    ```
+    <script src="Hangul.js"></script>
+    <script src="almondhangul.js"></script>
+    ```
+2. Define el input que recibe los caracteres españoles por medio del id __AlmondText__
+    ```
+    <input type="text" id="AlmondText">
+    ```
+3. Define el elemento que recibirá el valor de caracteres coreanos
+    ```
+    <h1 id="AlmondHangul"></h1>
+    ```
+### License
+----
+
+MIT
+
+
+**Free Software, Hell Yeah!**
--- almondhangul.js
+++ almondhangul.js
@@ -6,27 +6,69 @@
     }
 
     function parse() {
-        let chars = get_chars(this.value)
-        var output = []
+        let output = []
+        let chars = this.value
+
+        // console.log(chars)
         for (var i in chars) {
-            let char = chars[i]
-            let check_char = /^[ña-zA-Z]*$/g.test(char)
-            if ( check_char == true) {
-                let prev = chars[parseInt(i) - 1]
-                let next = chars[parseInt(i) + 1]
-                var is_vowel = isVowel(char)
-                if (is_vowel == false) {
-                    // var consonant = abc.consonants[char]
-                    let parsing = parseConsonant(char,prev,next)
-                    output = output.concat(parsing)
+          i = parseInt(i)
+          var char = chars[i]
+          var next = chars[i+1]
+          var prev = chars[i-1]
+          
+          var match = abc.matches(char)
+          if (match == 'vow') {
+  
+            const VOWELS = abc.vowels
+            if ('yw'.includes(char) && next != undefined && abc.matches(next) == 'vow') {
+              var vowel = VOWELS.get(char,next)
+            } else if ('yw'.includes(prev)) {
+              continue
+            } else {
+              if (char == 'u' && 'qg'.includes(prev)) {
+                if ('ei'.includes(next) || next == undefined) {
+                  continue
                 } else {
-                    let parsing = parseVowel(char,prev,next)
-                    output = output.concat(parsing)
+                  var vowel = VOWELS.get(char)
                 }
-            } else if (check_char == false) {
-                output.push(char)
+              } else {
+                var vowel = VOWELS.get(char)
+              }
             }
+  
+            var init = abc.vowels.init(char,prev,chars[i-2])
+            var parsed = [init,vowel]
+            output = output.concat(parsed)
+  
+  
+          } else if (match == 'cons') {
+            const CONSONANT = abc.consonants
+            if ('cg'.includes(char) && (abc.matches(next) == 'vow' || next == 'h')) {
+                var cons = CONSONANT.get(char,next)
+              } else if (char == 'h' && prev != 'c') {
+                continue
+              }
+               else {
+                var cons = CONSONANT.get(char)
+              }
+            
+            var init = CONSONANT.init(char,next)
+            if (prev != undefined && char == 'l' && abc.matches(next) == 'vow') {
+              var parsed = [cons,cons]
+            } else if ('ln'.includes(char)) {
+              var parsed = [cons]
+            } else {
+              var parsed = [cons,init]
+            }
+            output = output.concat(parsed)
+          }
+          
         }
+        output = output.filter((item) => { return item != null })
+        getHangul(output)
+      }
+
+      function getHangul(output) {
         let han = Hangul.assemble(output)
         let target_el = document.querySelector('#AlmondHangul')
         if (target_el != undefined) {
@@ -34,135 +76,130 @@
         } else {
             console.log('Falta añadir el id #AlmondHangul al elemento que contendrá el valor en coreano !!')
         }
-    }
-
-    function isVowel(char) {
-        let vowel = abc.vowels[char]
-        if (vowel == undefined) {
-            var result = false
-        } else {
-            var result = true
-        }
-        return result
-    }
-
-    function get_chars(input) {
-        let input_str = input.toLowerCase();
-        let flat_str = input_str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-        let chars = flat_str.split('')
-        return chars
-    }
-
-    function parseConsonant(char,prev,next) {
-        if (char == 'h') {
-            var parsed = []
-        } else {
-            let consonant = abc.consonants[char]
-            if (char == 'n') {
-                var parsed = [consonant]
-            } else {
-                var next_is_vowel = isVowel(next)
-                var prev_is_vowel = isVowel(prev)
-
-                var needs_init = false
-                if (next == undefined || next_is_vowel == false) {
-                    needs_init = true
-                }
-                
-                if (char == 'l' && next_is_vowel == true && prev_is_vowel == true) {
-                    var parsed = [consonant,consonant]
-                } else {
-                    var parsed = [consonant]
-                }
-    
-                if (needs_init == true && char != 'l' || needs_init == true && char == 'l' && prev_is_vowel == false ) {
-                    var init = abc.consonants.init
-                    parsed.push(init)
-                }
-            }
-        }
-        return parsed
-    }
-
-    function parseVowel(char,prev,next) {
-        var prev_is_vowel = isVowel(prev)
-        var next_is_vowel = isVowel(next)
-        var needs_init = false
-        if (prev == undefined || prev == 'h' || 'yw'.indexOf(prev) == -1 && prev_is_vowel == true) {
-            needs_init = true
-        }
-        if ('gq'.indexOf(prev) != -1 || 'yw'.indexOf(prev) != -1) {
-            var parsed = []
-        } else if ('yw'.indexOf(char) != -1) {
-            if (next_is_vowel == true) {
-                var vowel = abc.vowels[char][next]
-            } else {
-                var vowel = abc.vowels[char][char]
-            }
-            var parsed = [vowel]
-        } else {
-            let vowel = abc.vowels[char]
-            var parsed = [vowel]
-        }
-        
-        if (needs_init == true) {
-            var init = abc.vowels.init
-            parsed.unshift(init)
-        }
-        return parsed
-    }
-
-  
-
-  const abc = {
-          vowels: {
-            init: 'ㅇ',
-            a: 'ㅏ',
-            e: 'ㅔ',
-            i: 'ㅣ',
-            o: 'ㅗ',
-            u: 'ㅜ',
-            y: {
-                y: 'ㅣ',
-                a: 'ㅑ',
-                e: 'ㅖ',
-                i: 'ㅣ',
-                o: 'ㅛ',
-                u: 'ㅠ'
-            },
-            w: {
-                w: 'ㅜ',
-                a: 'ᅪ',
-                e: 'ㅞ',
-                i: 'ᅱ',
-                o: 'ㅝ',
-                u: 'ㅜ'
-            }
-          },
-          consonants: {
-              init: 'ㅡ',
-              b: 'ㅂ',
-              c: 'ㅋ',
-              ch: 'ㅊ',
-              d: 'ㄷ',
-              f: 'ㅍ',
-              g: 'ㄱ',
-              j: 'ㅎ',
-              k: 'ㅋ',
-              l: 'ㄹ',
-              m: 'ㅁ',
-              n: 'ㄴ',
-              ñ: '니',
-              p: 'ㅍ',
-              q: 'ᄏ',
-              r: 'ㄹ',
-              s: 'ㅅ',
-              t: 'ㅌ',
-              v: 'ㅂ',
-              x: '크',
-              z: 'ㅉ'
-          }
       }
+
+      const abc = {
+        matches: function(char) {
+          var vows = Object.keys(abc.vowels)
+          var consn = Object.keys(abc.consonants)
+          if (vows.includes(char) == true) {
+            return 'vow'
+          } else if (consn.includes(char) == true) {
+            return 'cons'
+          }
+        },
+         vowels: {
+              get: function(char,next) {
+                  next = next || char
+                  if (typeof this[char] == 'object') {
+                    return this[char][next]
+                  } else if (typeof this[char] == 'string') {
+                    return this[char]
+                  }
+              },
+              init: function(char,prev,qg) {
+                if (prev == undefined) {
+                  return 'ㅇ'
+                } else if (prev == 'u' && 'qg'.includes(qg)) {
+                  if ('aou'.includes(char)) {
+                    return 'ㅇ'
+                  } else if ('ei'.includes(char)) {
+                    return null
+                  }
+                } else if (char == 'u' && 'qg'.includes(prev)) {
+                  return null
+                } else if (prev == 'h') {
+                  if (qg == 'c') {
+                    return null
+                  } else {
+                    return 'ㅇ'
+                  }
+                } else if (abc.matches(prev) == 'vow') {
+                  return 'ㅇ'
+                } else {
+                  return null
+                }
+              },
+              a: 'ㅏ',
+              e: 'ㅔ',
+              i: 'ㅣ',
+              o: 'ㅗ',
+              u: 'ㅜ',
+              ü: 'ㅜ',
+              y: {
+                  y: 'ㅣ',
+                  a: 'ㅑ',
+                  e: 'ㅖ',
+                  i: 'ㅣ',
+                  o: 'ㅛ',
+                  u: 'ㅠ'
+              },
+              w: {
+                  w: 'ㅜ',
+                  a: 'ᅪ',
+                  e: 'ㅞ',
+                  i: 'ᅱ',
+                  o: 'ㅝ',
+                  u: 'ㅜ',
+                  ü: 'ㅜ'
+              }
+            },
+            consonants: {
+                get: function(char,next) {
+                  next = next || char
+                  if (typeof this[char] == 'object') {
+                      return this[char][next]
+                  } else if (typeof this[char] == 'string') {
+                    return this[char]
+                  }
+                },
+                init: function(char,next) {
+                    if (next == undefined || abc.matches(next) == 'cons') {
+                      return 'ㅡ'
+                    } else if ('nl'.includes(char)) {
+                      return null
+                    } else {
+                      return null
+                    }
+                },
+                b: 'ㅂ',
+                c: {
+                    c: 'ㅋ',
+                    a: 'ㅋ',
+                    e: 'ㅅ',
+                    i: 'ㅅ',
+                    o: 'ㅋ',
+                    u: 'ㅋ',
+                    h: 'ㅊ'
+                },
+                d: 'ㄷ',
+                f: 'ㅍ',
+                g: {
+                    g: 'ㄱ',
+                    a: 'ㄱ',
+                    e: 'ㅎ',
+                    i: 'ㅎ',
+                    o: 'ㄱ',
+                    u: 'ㄱ', 
+                    ü: 'ㄱ'
+                },
+                j: 'ㅎ',
+                k: 'ㅋ',
+                l: 'ㄹ',
+                m: 'ㅁ',
+                n: 'ㄴ',
+                ñ: '니',
+                p: 'ㅍ',
+                q: 'ㅋ',
+                r: 'ㄹ',
+                s: 'ㅅ',
+                t: 'ㅌ',
+                v: 'ㅂ',
+                x: ['크','ㅅ'],
+                z: 'ㅉ'
+            }
+        }
+
  if(typeof(window.myWindowGlobalLibraryName) === 'undefined'){
     window.myWindowGlobalLibraryName = AlmondHangul();
   }
